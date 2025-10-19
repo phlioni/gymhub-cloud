984c5ea639a1adb82b7ebb33d284f49ee79d57f1
phlioni <ph.lioni@hotmail.com>
2025-10-19 13:29:07 -0300

    feito2

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -203,7 +203,7 @@ const SidebarContent = ({ data, onLinkClick }: { data: AppLayoutData | null; onL
                     onClick={handleSignOut}
                 >
                     <LogOut className="h-5 w-5 mr-3" />
-                    <span>Sai</span>
+                    <span>Sair</span>
                 </Button>
             </div>
         </>
